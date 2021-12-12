ace4b93c6e88d78d93e4a27b971f67490e441fb6
Altaha <altahabinsyafriadi@gmail.com>
2021-12-13 02:05:19 +0700

    create login box for admin

---
+++ fe/src/admin/AdminLogin.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../style/style.css";
+
+const AdminLogin = () => {
+  return (
+    <div className="admin-bg-login">
+      <div className="loginbox"></div>
+    </div>
+  );
+};
+
+export default AdminLogin;
