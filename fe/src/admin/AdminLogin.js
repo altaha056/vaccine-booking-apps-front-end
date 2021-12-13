5438590709ea1bdcbef4626265304c8f3fb7cb6b
Altaha <altahabinsyafriadi@gmail.com>
2021-12-13 13:48:07 +0700

    add style for input form

--- fe/src/admin/AdminLogin.js
+++ fe/src/admin/AdminLogin.js
@@ -1,10 +1,31 @@
 import React from "react";
 import "../style/style.css";
-
+import Logo from "../style/logo.svg";
 const AdminLogin = () => {
   return (
     <div className="admin-bg-login">
-      <div className="loginbox"></div>
+      <div className="loginbox">
+        <div className="grid-container-admin">
+          <div className="login-header">
+            <h1>Login</h1>
+          </div>
+          <div className="login-logo">
+            <img src={Logo} alt="logo" />
+          </div>
+          <div className="login-form">
+            <form>
+              <div className="inputbox">
+                <input type="text" required />
+                <label>Email</label>
+              </div>
+              <div className="inputbox">
+                <input type="password" required />
+                <label>Password</label>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
