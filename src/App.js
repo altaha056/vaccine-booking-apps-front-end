4cf87ba32f222f305d334a8d2c59eedaae7a238a
Altaha <altahabinsyafriadi@gmail.com>
2021-12-17 23:42:46 +0700

    creating react router no match

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import AdminAddVaccination from "./admin/AdminAddVaccination";
 import AdminNews from "./admin/AdminNews";
 import AdminProfile from "./admin/AdminProfile";
 import AdminEditVaccination from "./admin/AdminEditVaccination";
+import NoMatch from "react-router-nomatch";
 function App() {
   return (
     <>
@@ -25,6 +26,7 @@ function App() {
             element={<AdminEditVaccination />}
           />
           <Route path="/admin/test" element={<AdminNews />} />
+          <Route element={<AdminLogin />} />
         </Routes>
       </Router>
     </>
